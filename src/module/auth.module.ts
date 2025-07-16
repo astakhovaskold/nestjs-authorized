312d02baab2b5de84701bcda2d981acc4037a416
Askold Astakhov <astakhovaskold@gmail.com>
2025-07-16 15:12:25 +0300

    auth module

--- src/module/auth.module.ts
+++ src/module/auth.module.ts
@@ -1,4 +1,39 @@
-import { Module } from '@nestjs/common';
+import { Module, DynamicModule, Provider, Global } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtService, JwtModuleOptions } from '../services/jwt.service';
+import { JwtStrategy } from '../strategies/jwt.strategy';
+import { RefreshTokenStrategy } from '../strategies/refresh-token.strategy';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import { RefreshTokenGuard } from '../guards/refresh-token.guard';
+import { IUserService } from '../interfaces/user-service.interface';
 
+@Global()
 @Module({})
-export class AuthModule {}
+export class AuthModule {
+  static forRoot(options: JwtModuleOptions, userServiceProvider: Provider<IUserService>): DynamicModule {
+    return {
+      module: AuthModule,
+      imports: [
+        JwtModule.register({}), // Используем только для DI JwtService
+      ],
+      providers: [
+        { provide: 'JWT_MODULE_OPTIONS', useValue: options },
+        JwtService,
+        JwtStrategy,
+        RefreshTokenStrategy,
+        JwtAuthGuard,
+        RefreshTokenGuard,
+        userServiceProvider,
+      ],
+      exports: [
+        JwtService,
+        JwtStrategy,
+        RefreshTokenStrategy,
+        JwtAuthGuard,
+        RefreshTokenGuard,
+        userServiceProvider,
+      ],
+      global: true,
+    };
+  }
+}
